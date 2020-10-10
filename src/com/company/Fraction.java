c5e9ad3402df70c38dc7154728186dfb99837330
aymen <aymen.chihani@univ-tebessa.dz>
2020-10-10 09:14:46 -0700

    update 1

---
+++ src/com/company/Fraction.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Fraction {
+
+    public Integer meth1(int a,int b){
+        return(a/b);
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Fraction f=new Fraction();
+	System.out.println(f.meth1(10,5));
+
+    }
+}
