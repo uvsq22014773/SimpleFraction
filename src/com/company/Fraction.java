5cf58ec0c6a8874ede348af588ffb76c1f4bdbb6
uvsq22014773 <aymen.chihani@univ-tebessa.dz>
2020-10-12 04:18:47 -0700

    mend

--- src/com/company/Fraction.java
+++ src/com/company/Fraction.java
@@ -3,7 +3,7 @@ package com.company;
 public class Fraction {
 
     public String meth1(int a,int b){
-        return("Resulta:"+Integer.toString(a/b));
+        return("Resultat:"+Integer.toString(a/b));
 
     }
 }
