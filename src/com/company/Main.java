4ec94e18ad510640cdcd68f5a80392663da5ea28
uvsq22014773 <aymen.chihani@univ-tebessa.dz>
2020-10-12 13:44:15 -0700

    update3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Fraction f=new Fraction();
-	System.out.println(f.meth1(10,5));
+        Fraction f=new Fraction();
+        System.out.println(f.meth1(10,5));
 
     }
 }
