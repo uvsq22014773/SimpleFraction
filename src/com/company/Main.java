bdc54935becb0fa234c38d8b896396cbd4873620
uvsq22014773 <aymen.chihani@univ-tebessa.dz>
2020-10-17 10:58:51 -0700

    update3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Main {
-
+String m="uneVariable";
     public static void main(String[] args) {
         Fraction f=new Fraction();
         System.out.println(f.meth1(10,5));
