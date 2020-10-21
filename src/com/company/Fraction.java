84ce474db1ad4864e73e145df0ecc8b6ec5686ae
uvsq22014773 <aymen.chihani@univ-tebessa.dz>
2020-10-21 12:09:36 -0700

    update1

--- src/com/company/Fraction.java
+++ src/com/company/Fraction.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.StringTokenizer;
+
 public class Fraction {
 
     public String meth1(int a,int b){
         return("Resulta:"+Integer.toString(a/b));
+
+
 
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,9 @@ package com.company;
 
 public class Main {
 String m="uneVariable";
+
     public static void main(String[] args) {
-        Fraction f=new Fraction();
+ Fraction f=new Fraction();
         System.out.println(f.meth1(10,5));
 
     }
